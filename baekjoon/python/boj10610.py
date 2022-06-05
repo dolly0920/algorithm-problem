e2cf95cd3cb323cbea485e816a3bca862c8a28b8
dolly0920 <dolly0920@naver.com>
2022-06-05 16:42:49 +0900

    boj10610 python

---
+++ baekjoon/python/boj10610.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.stdin = open("input.txt", "rt")
+
+if __name__ == "__main__" :
+    n = list(input())
+    n.sort(reverse = True)
+    sum = 0
+    for i in n :
+        sum += int(i)
+    if sum % 3 != 0 or "0" not in n :
+        print(-1)
+    else :
+        print("".join(n))
