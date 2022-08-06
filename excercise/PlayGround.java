4badd2192f40126155ad0fd38da08b865958b92a
dolly0920 <dolly0920@naver.com>
2022-08-06 13:30:05 +0900

    Implement new playground class

--- excercise/PlayGround.java
+++ excercise/PlayGround.java
@@ -1,20 +1,8 @@
 package excercise;
 
-import java.util.*;
-
 public class PlayGround {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-        List<String> test = new ArrayList<>(Arrays.asList("c", "b", "a"));
-
-        System.out.println(test);
-
-        Collections.sort(test, (a, b) -> {
-            return (a+b).compareTo(b+a); // compareTo : 입력값보다 크면 1, 같으면 0, 작으면 -1
-        });
-
-        System.out.println(test);
+        System.out.println("hello world!");
     }
 }
